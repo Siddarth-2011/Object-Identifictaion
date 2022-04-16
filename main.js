910acbd1a1e4624833620b626226ad1a5d803545
Siddarth-2011 <81513284+Siddarth-2011@users.noreply.github.com>
2022-04-16 09:37:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+img = "";
+status="";
+objects = [];
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380.380);
+    video.hide();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+    
+
+    if(status !="") 
+    {
+        for(i = 0; i< objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded()
+{
+  console.log("Model Loaded!")
+  status = true;
+  objectDetector.detect(video, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
